131a95c354cee51e3568a3d4f9af322be6c74fe8
Hariel <hrlcosta@hotmail.com>
2026-03-23 17:12:37 -0300

    Segunda alteração

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print ('olá mundo')
+print ("1. Cadastrar restaurante")
+print ("2. Listar restaurante")
+print ("3. Ativar restaurante")
+print ("4. Sair\n")
+
+opcao_escolhida = input ("Escolha uma opção: ")
+print("Você escolheu a opção", opcao_escolhida)
