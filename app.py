53756d97ff59abdd48306c818c339666ba579bd8
Hariel <hrlcosta@hotmail.com>
2026-03-23 17:16:40 -0300

    Terceira alteração

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ print ("3. Ativar restaurante")
 print ("4. Sair\n")
 
 opcao_escolhida = input ("Escolha uma opção: ")
-print("Você escolheu a opção", opcao_escolhida)
+print(f"Você escolheu a opção {opcao_escolhida}!")
