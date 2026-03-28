c3c85ede51e97263ee46c0c3c5c9be0885eb40de
Hariel <hrlcosta@hotmail.com>
2026-03-28 19:23:17 -0300

    Oitava alteração

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def exibir_nome_do_programa():
 """)
 
 
-
 def exibir_opcoes():
     print ("1. Cadastrar restaurante")
     print ("2. Listar restaurante")
@@ -22,20 +21,31 @@ def finalizar_app():
     os.system("cls")
     print("Finalizando o app\n")
 
+def opcao_invalida():
+    print("Opção inválida\n")
+    input("Digite uma tecla para voltar ao menu principal")
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
-    # opcao_escolhida = int(opcao_escolhida) // outra opção
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+        # opcao_escolhida = int(opcao_escolhida) // outra opção
 
-    if opcao_escolhida == 1:
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3:
-        print("Ativar restaurante")
-    else:
-        finalizar_app()
-
+        if opcao_escolhida == 1:
+            print("Cadastrar restaurante")
+        elif opcao_escolhida == 2:
+            print("Listar restaurantes")
+        elif opcao_escolhida == 3:
+            print("Ativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+        
 def main():
+    os.system("cls")
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
