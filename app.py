dda7bd5d1196015c2a16e7fa7e7538ce7a6ec701
Hariel <hrlcosta@hotmail.com>
2026-03-28 19:01:58 -0300

    Sexta alteração

--- app.py
+++ app.py
@@ -3,32 +3,40 @@ import os
 def exibir_nome_do_programa():
     print ("SABOR EXPRESS")
 
-print ("1. Cadastrar restaurante")
-print ("2. Listar restaurante")
-print ("3. Ativar restaurante")
-print ("4. Sair\n")
+# deixei assim porque ele usou uma fonte grande no nome
+# /
+# /
+# /
+# /
+# /
 
-opcao_escolhida = int(input("Escolha uma opção: "))
-# opcao_escolhida = int(opcao_escolhida) // outra opção
+def exibir_opcoes():
+    print ("1. Cadastrar restaurante")
+    print ("2. Listar restaurante")
+    print ("3. Ativar restaurante")
+    print ("4. Sair\n")
 
 def finalizar_app():
     os.system("cls")
     print("Finalizando o app\n")
 
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    # opcao_escolhida = int(opcao_escolhida) // outra opção
 
-
-
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes")
-elif opcao_escolhida == 3:
-    print("Ativar restaurante")
-else:
-    finalizar_app()
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
-
+    exibir_opcoes()
+    escolher_opcao()
+    
 if __name__ == "__main__":
     main()
